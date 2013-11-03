1dbbbec18c02dbf71855b2a68e7f4c764ea92451
ghostnotes <ghostnotesdot@gmail.com>
2013-11-03 17:02:21 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Videos2::Application.config.secret_key_base = '23bbff507a9c7be97f7675253df104f507ff05b08e3693842d007d03e2ea278a16660a3ef678668e57f11c8cf7458667df41ee42c4570a6103d33aa30c6660de'
